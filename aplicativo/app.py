b2a9a8ad29e7528b63210b3a219382d683f46fca
Brunno Bronner <brunnobronner@engenharia.grupomassa.com.br>
2024-10-09 14:08:06 -0300

    atualizações do projeto: V2

--- aplicativo/app.py
+++ aplicativo/app.py
@@ -6,32 +6,37 @@ import io
 sg.theme("topanga") #tema
 
 
+layout = [ [sg.Push(), sg.Text('Cole o seu link aqui'), sg.Push()],
+          [sg.InputText(key = '-LINK-')],
+          [sg.Button('Gerar seu qr code', key= '-BOTAO-', pad= (20,10), Row=(1))],
+          [sg.Button('Sair', key = '-SAIR-', pad=(10,5), font=('helvética'), Row=(1))],
+          [sg.Image(key='-IMAGE-')]
+          ]
 
-layout = [ [sg.Text('Insira seu link aqui! ')], #elementos da página
-            [sg.InputText( key= '-LINK-')],
-            [sg.Button('Gerar seu Qr Code',)],
-            [sg.Button('sair')],
-            [sg.Image(key = '-IMAGE-')]]
-
-wind = sg.Window ("gerador de cpf",layout) #inicializador (talvez)
-
-
+janela1 = sg.Window('Gerador de Qr Code', layout)
+ 
+ 
+ 
 while True:
-    events, values = wind.read()
     
-    if events == sg.WINDOW_CLOSED or events == 'sair':
-                break
-            
-    elif events == 'Gerar seu Qr Code':
+    events, values = janela1.read()
+    
+    if events == sg.WINDOW_CLOSED or events =='-SAIR-': #verificar do porque o erro
+        break
+    elif events == '-BOTAO-':
         link = values['-LINK-']
-        
         if link:
             qr = qrcode.make(link)
-            sg.popup('qr code gerado com sucesso!')
-            byt = io.BytesIO()
-            qr.save(byt, format ='PNG')
-            byt.seek(0)
+            byte_1 = io.BytesIO()
+            qr.save( byte_1, format='PNG')
+            byte_1.seek(0)
             
-            wind['-IMAGE-'].update(data=byt.getvalue())
+            janela1['-IMAGE-'].update(data= byte_1.getvalue())
+            sg.popup('qr code gerado com sucesso!')
+        else: 
+            sg.popup_cancel('insira um valor válido')
+             
+        
         
 
+janela1.close()
